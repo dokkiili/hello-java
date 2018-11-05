250f5b662d8d504513fe36ff00304cbfb7a6e6da
Dokkii Li <dokkiivfs@outlook.com>
2018-11-05 16:19:31 +0800

    Plan changed..on Hello.Java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Joe,Java");
 		System.out.println("Joe,Java");
-
+		System.out.print("I got a new plan....")
 	}
 	
 
